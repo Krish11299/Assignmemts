c20a85c37121a63f7899b1de4fd475b068a7e9b2
Krish11299 <107124420+Krish11299@users.noreply.github.com>
2022-06-21 18:17:30 +0530

    Delete String_Assign directory

--- String_Assign/StringLength.java
+++
@@ -1,8 +0,0 @@
-package String_Assign;
-
-public class StringLength {
-public static void main(String[] args) {
-	String s="Hello World";
-	System.out.println("String Length is: "+s.length());
-}
-}
